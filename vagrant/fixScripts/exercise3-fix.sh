6b97693f9d45fa91ee3761d12425abb831b57561
lirany1 <liran80@gmail.com>
2017-11-04 18:13:34 +0200

    Update exercise3-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-#add fix to exercise3 here
+#add fix to exercise3 here got a error when trying to get to the url :Forbidde You don't have permission to access / on this server.
+#Apache/2.2.22 (Ubuntu) Server at 192.168.100.10 Port 80 after the fix we have a message : Hello world
+sudo sed -i -e '0,/deny from all/ s/deny from all/allow from all/' /etc/apache2/sites-available/default
+sudo service apache2 restart
+sudo service apache2 start-htcacheclean
