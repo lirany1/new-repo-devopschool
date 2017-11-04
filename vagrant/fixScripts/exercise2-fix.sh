a6eb3afecb7927b01271b29bf44f5b4f9d168d2f
lirany1 <liran80@gmail.com>
2017-11-04 18:11:22 +0200

    Update exercise2-fix.sh

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise2 here
+#You don't have permission to access /ascii/ab/007.txt on this server- we need to delete the wrong host 
+sudo sed -i '/127.0.0.1 www.ascii-art.de/d' /etc/hosts
