19aa86309f0ecaf100df65bb06312b8b4bb3b482
lirany1 <liran80@gmail.com>
2017-11-04 18:10:50 +0200

    Update exercise1-fix.sh

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-#add fix to exercise1 here
+#add fix to exercise1 here- #  curl: (7) couldn't connect to host The program 'curl' is currently not installed.  You can install it by typing:sudo apt-get install curl
+#we are using apt-get install with all prompts and run non-interactively.
+sudo apt-get -y install curl
