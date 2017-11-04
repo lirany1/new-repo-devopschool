c4b7988edb7d036705b9ed566ab546a83c4a6f0d
lirany1 <liran80@gmail.com>
2017-11-04 18:16:08 +0200

    Update exercise5-fix_server2.sh

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+sudo su
+sudo touch /home/vagrant/.ssh/id_rsa.pub
+sudo chmod 700 /home/vagrant/.ssh/*
+sudo chown vagrant:vagrant /home/vagrant/.ssh/*
+sudo ssh-keygen -t rsa
+cat .ssh/id_rsa.pub | ssh server2 'cat >> .ssh/authorized_keys'
+sudo apt-get install sshpass
+
